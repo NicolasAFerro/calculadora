851448effecfaf0b8f6ff9d6b481c1f7d7f9af59
NicolasAFerro <nicolasalexandrinoferro@gmail.com>
2024-07-23 20:46:35 -0300

    closes #5, closes #6

--- javascript.js
+++ javascript.js
@@ -1,21 +1,29 @@
 function calc(operacao) {
   var num2 = parseFloat(document.getElementById("num2").value);
   var num1 = parseFloat(document.getElementById("num1").value);
-
+  var saida = 0;
   switch (operacao) {
     case "+":
-      var saida = num1 + num2;
+      saida = num1 + num2;
       break;
     case "-":
-      var saida = num1 - num2;
+      saida = num1 - num2;
       break;
     case "*":
-      var saida = num1 * num2;
+      saida = num1 * num2;
       break;
     case "/":
-      var saida = num1 / num2;
+      saida = num1 / num2;
       break;
   }
 
   document.getElementById("output").value = saida;
+  var newOperation =
+    "<p>" + num1 + " " + operacao + " " + num2 + " = " + saida + "</p>";
+  var history = document.getElementById("history");
+
+  history.innerHTML = newOperation + history.innerHTML;
+  if (history.children.length > 10) {
+    history.removeChild(history.childNodes[10]);
+  }
 }
