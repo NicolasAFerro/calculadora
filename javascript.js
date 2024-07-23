05c28ae3900aea46987ce7e214b8a850bd96155e
NicolasAFerro <nicolasalexandrinoferro@gmail.com>
2024-07-22 21:09:59 -0300

    closes #4 - operacoes matematicas

--- javascript.js
+++ javascript.js
@@ -1,8 +1,21 @@
-function calc() {
+function calc(operacao) {
   var num2 = parseFloat(document.getElementById("num2").value);
   var num1 = parseFloat(document.getElementById("num1").value);
 
-  var saida = num1 + num2;
+  switch (operacao) {
+    case "+":
+      var saida = num1 + num2;
+      break;
+    case "-":
+      var saida = num1 - num2;
+      break;
+    case "*":
+      var saida = num1 * num2;
+      break;
+    case "/":
+      var saida = num1 / num2;
+      break;
+  }
 
   document.getElementById("output").value = saida;
 }
